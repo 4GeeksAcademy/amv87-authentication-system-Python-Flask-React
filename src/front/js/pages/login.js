5724d0ba4853cc37ac46c44ac3992c72543831d4
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-18 20:12:39 +0000

    added src/front/js/pages/login.js

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container mt-5">
+            <div className="col-6">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <Link to="/private">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+					</Link>
+                </form>
+            </div>
+		</div>
+	);
+};
