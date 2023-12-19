d7c79818a79b5ec018338b1dc6e080ce99c70d3a
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-19 18:41:01 +0000

    cleared home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import Homepic from "../../img/home.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -10,6 +10,7 @@ export const Home = () => {
 		<div className="text-center mt-5">
 			<h1>Hello!!</h1>
 			<br></br>
+			<img src={Homepic} className="img-fluid rounded mb-4" alt="..."></img>
 			<p>Welcome.</p>
 			<p>Please sign up or create a new user.</p>
 		</div>
