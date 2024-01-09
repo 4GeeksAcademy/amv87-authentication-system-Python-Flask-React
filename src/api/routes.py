ae7bf91d0db5dc5d5a5a656be2e1f06907c4122a
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-09 20:26:22 +0000

    created POST user

--- src/api/routes.py
+++ src/api/routes.py
@@ -36,3 +36,15 @@ def get_user(user_id):
     results = User.query.filter_by(id=user_id).first()
 
     return jsonify(results.serialize()), 200
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    request_body = request.get_json(force=True)
+    new_user = User(email=request_body["email"], password=request_body["password"], is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    response_body = {
+        'msg': 'Your user has been added.'
+    }
+
+    return jsonify(response_body), 201
