12b9a01aa403ae35ce01be9a4a341d2e118c9886
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-12 16:57:25 +0000

    added memory to login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,25 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function sendData(e){
+        e.preventDefault()
+        actions.login(email, password)
+    }
 
 	return (
 		<div className="container mt-5">
             <div className="col-md-6">
                 <form>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <label htmlFor="emailInput" className="form-label">Email address</label>
+                        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <label htmlFor="passwordInput" className="form-label">Password</label>
+                        <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="form-control" id="passwordInput" />
                     </div>
                     <Link to="/private">
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
 					</Link>
                 </form>
             </div>
