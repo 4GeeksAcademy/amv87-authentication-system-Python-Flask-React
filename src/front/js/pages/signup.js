9e8f22b726f90cf686849aca58ef7f32b76e559e
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-13 18:46:25 +0000

    added navigation

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const Signup = () => {
 	const { store, actions } = useContext(Context)
@@ -16,22 +17,24 @@ export const Signup = () => {
 
 	return (
 		<div className="container mt-5">
-            <div className="col-md-6">
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="emailInput" className="form-label">Email address</label>
-                        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="passwordInput" className="form-label">Password</label>
-                        <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="form-control" id="passwordInput" />
-                    </div>
-                    <Link to="/private">
-                        <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
-					</Link>
-                </form>
-            </div>
+            {store.auth == true ? <Navigate to="/Private" /> :
+                <div className="col-md-6">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="emailInput" className="form-label">Email address</label>
+                            <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="passwordInput" className="form-label">Password</label>
+                            <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="form-control" id="passwordInput" />
+                        </div>
+                        <Link to="/private">
+                            <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
+                        </Link>
+                    </form>
+                </div>
+            }
 		</div>
 	);
 };
